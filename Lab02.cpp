1aa7e0696f29a58c0bda66314e04da2d914f698d
Adamacy <a.jakubiak.srem@gmail.com>
2025-03-18 17:50:28 +0100

    Added time format and changed .gitignore

--- Lab02.cpp
+++ Lab02.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+class Time {
+private:
+    int seconds_;
+    int minutes_;
+    int hours_;
+public:
+    Time(int h, int m, int s) {
+        hours_ = ((m / 60) + h) % 24;
+        minutes_ = ((s / 60) + m) % 60;
+        seconds_ = s % 60;
+    }
+    Time(int s) {
+        int minutes = (s / 60);
+        hours_ = minutes / 60;
+        seconds_ = s % 60;
+        minutes_ = minutes % 60;
+    }
+
+    friend std::ostream& operator<<(std::ostream& str, Time& t);
+};
+std::ostream& operator<<(std::ostream& str, Time& t) {
+    if (t.hours_ != 0) {
+        str << t.hours_ << "h:" << t.minutes_ << "m:" << t.seconds_ << "s\n";
+    }
+    else {
+        str << t.minutes_ << "m:" << t.seconds_ << "s\n";
+    }
+    return str;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Time t1(13, 62, 73);
+    std::cout << t1;
 }
